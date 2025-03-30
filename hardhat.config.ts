208102dc3c670c359d9ea390b3d7a57ff89b743d
innoinno31 <franck.rp@outlook.com>
2025-03-31 00:51:42 +0200

    WIP - nuclear cert contract.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+  },
 };
 
 export default config;
